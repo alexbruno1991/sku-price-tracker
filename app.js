4e90dc820c982d73c3de33be1087164f8a9396c9
alexbruno1991 <alex.r.bruno@gmail.com>
2024-05-20 16:36:41 -0700

    Update app.js

--- app.js
+++ app.js
@@ -31,8 +31,11 @@ function checkAmazonPrice(asin, mapPrice) {
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
     const amazonUrl = `https://www.amazon.com/dp/${asin}`;
     
+    console.log(`Fetching price for ASIN: ${asin} with MAP Price: $${mapPrice}`);
+    
     axios.get(proxyUrl + amazonUrl)
         .then(response => {
+            console.log('Response received from Amazon:', response);
             const parser = new DOMParser();
             const doc = parser.parseFromString(response.data, 'text/html');
             const priceElement = doc.querySelector('#priceblock_ourprice, #priceblock_dealprice');
